76f3c63f108ee55945db19c826b81030bcd634b5
Ales-Slany <ales.slany@gybnp.cz>
2025-01-10 15:08:22 +0100

    Seru na pravidla

--- app/app.py
+++ app/app.py
@@ -17,12 +17,6 @@ except OSError:
 
 db.init_app(app)
 
-#Argumenty/pravidla pro potrebna data pri tvorbe hry (?)
-hra_post_arg = reqparse.RequestParser()
-hra_post_arg.add_argument("name", type=str, help="Pojmenování hry", required=True)
-hra_post_arg.add_argument("difficulty", type=str, help="Úroveň obtížnosti hry", required=True)
-hra_post_arg.add_argument("board", help="Stav mřížky hry", required=True)
-
 @app.route('/')
 def hello_world():  # put application's code here
     return "Hello TdA"
@@ -35,9 +29,8 @@ def get_organization():
 #Prvni verze pro API - vytvoreni hry
 @app.route('/api/v1', methods=['POST'])
 def create_game():
-    argumenty = hra_post_arg.parse_args()
     game_data = request.get_json()
-    return (argumenty), 201 #Z nejakyho duvodu vraci jen prvni seznam ze seznamu mrizky, nevim a dneska uz neresim
+    return {'game_data': game_data}, 201 #Z nejakyho duvodu vraci jen prvni seznam ze seznamu mrizky, nevim a dneska uz neresim
 
 if __name__ == '__main__':
     app.run(debug=True) 
